18c7d0a04dd5bbe9a52d5fd30c64f289728fe121
sgs <sgs@debian-sgs>
2017-01-15 11:26:18 +0800

    New readme: <2017-01-15 Sun 11:26:39>

--- README.md
+++
@@ -1,2 +0,0 @@
-# iss
-iss-get-account-login-获取网页里的账户并自动连接: &lt;2017-01-14 Sat 21:35:14>
---
+++ iss.sh
@@ -0,0 +1,3 @@
+/usr/bin/sbcl --load /home/sgs/Common-Lisp/iss-auto/iss.lisp
+# auto load Lisp file when Linux boots
+# copy this file to /etc/profile.d/
