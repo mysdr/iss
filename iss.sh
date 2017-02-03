bfed0a639ed868fdfd54f054891ddafe2ab9d5dd
sgs-site <mastermeichen@yahoo.com>
2017-02-03 08:32:37 +0000

    # 新增 删除文件 <2017-02-03 Fri 16:32:22>

--- iss.sh
+++ iss.sh
@@ -1,6 +1,8 @@
+# 新增 删除文件 <2017-02-03 Fri 16:32:22>
 # 新增 删除相关文件夹 <2017-02-03 Fri 16:30:13>
 sudo apt-get install shadowsocks sbcl
 cd /tmp/
+rm iss.sh
 rm -r /tmp/iss
 git clone https://github.com/sgs-site/iss.git
 rm -r ~/iss
