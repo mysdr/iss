f92fe7988af5e81733a4f8686db7a4e4cc8cf0da
sgs-site <mastermeichen@yahoo.com>
2017-02-04 09:52:17 +0800

    # 新增 crontab定时更新 <2017-02-04 六 09:50:59>
    crontab ~/iss/iss-crontab

--- iss.sh
+++ iss.sh
@@ -1,3 +1,4 @@
+# 新增 crontab 定时更新 <2017-02-04 六 09:50:59>
 # 新增 删除文件 <2017-02-03 Fri 16:32:22>
 # 新增 删除相关文件夹 <2017-02-03 Fri 16:30:13>
 sudo apt-get install shadowsocks sbcl
@@ -14,3 +15,5 @@ cd ~/iss
 # auto load Lisp file when Linux boots
 # copy this file to /etc/profile.d/
 # or copy this to /etc/rc.local
+
+crontab ~/iss/iss-crontab
