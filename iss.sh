c4242671b4ef63e64539c76f8cce0191adad48ed
sgs-site <mastermeichen@yahoo.com>
2017-02-04 09:56:01 +0800

    # 优化 强制删除文件 <2017-02-04 六 09:54:44>
    rm -rf /tmp/iss

--- iss.sh
+++ iss.sh
@@ -1,14 +1,15 @@
+# 优化 强制删除文件 <2017-02-04 六 09:54:44>
 # 新增 crontab 定时更新 <2017-02-04 六 09:50:59>
 # 新增 删除文件 <2017-02-03 Fri 16:32:22>
 # 新增 删除相关文件夹 <2017-02-03 Fri 16:30:13>
 sudo apt-get install shadowsocks sbcl
 cd /tmp/
 rm iss.sh
-rm -r /tmp/iss
+rm -rf /tmp/iss
 git clone https://github.com/sgs-site/iss.git
-rm -r ~/iss
+rm -rf ~/iss
 mkdir ~/iss
-cp -r /tmp/iss/* ~/iss
+cp -rf /tmp/iss/* ~/iss
 cd ~/iss
 /usr/bin/sbcl --load iss.lisp
 
